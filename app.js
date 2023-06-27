f225104f6c2c4e712640886e29b957f731b7ad02
Ashley Mical <ashleymmical@gmail.com>
2023-06-26 20:21:35 -0500

    corrected js logic

--- app.js
+++ app.js
@@ -1,77 +1,64 @@
 let choiceOptions = ["Rock", "Paper", "Scissors"];
-let userChoice = "";
-let userScore = 0;
+let playerScore = 0;
 let computerScore = 0;
-let cont = 'y';
+let roundCounter = 0;
 
+function getComputerChoice() {
+    return choiceOptions[Math.floor(Math.random() * choiceOptions.length)];
+}
 
-console.log('Time to play ROCHAMBEAU!');
+function playRound() {
+    let playerSelection = parseInt(prompt('Type in the number of your choice: (1) Rock, (2) Paper, or (3) Scissors'));
 
-while (cont === 'y') {
     while (true) {
-        try {
-            let choiceNumber = parseInt(prompt('Type in the number of your choice: (1) Rock, (2) Paper, or (3) Scissors'));
-
-            if (choiceNumber === 1) {
-                userChoice = 'Rock';
-                break;
-            } else if (choiceNumber === 2) {
-                userChoice = 'Paper';
-                break;
-            } else if (choiceNumber === 3) {
-                userChoice = 'Scissors';
-                break;
-            } else {
-                console.log('¯\\(°_o)/¯');
-                console.log('Huh? Your choice must be a number 1-3.');
-            }
-        } catch (e) {
-            console.log('¯\\(°_o)/¯');
-            console.log('Huh? Your choice must be a number 1-3.');
-        }
-    }
-
-    console.log(`You chose ${userChoice}.`);
-
-    let computerChoice = choiceOptions[Math.floor(Math.random() * choiceOptions.length)];
-
-    console.log(`The computer chose ${computerChoice}`);
-
-    if (userChoice === computerChoice) {
-        console.log("IT'S A TIE!");
-        console.log('***CURRENT SCORE***\n' +
-            'You: ' + userScore + '\n' +
-            'Computer: ' + computerScore + '\n');
-    } else if (userChoice === 'Rock' && computerChoice === 'Scissors' || userChoice === 'Paper' && computerChoice === 'Rock' || userChoice === 'Scissors' && computerChoice === 'Paper') {
-        console.log('YOU WON!');
-        userScore++;
-        console.log('***CURRENT SCORE***\n' +
-            'You: ' + userScore + ' *\n' +
-            'Computer: ' + computerScore + '\n');
-    } else {
-        console.log('THE COMPUTER WON!');
-        computerScore++;
-        console.log('***CURRENT SCORE***\n' +
-            'You: ' + userScore + '\n' +
-            'Computer: ' + computerScore + '*\n');
-    }
-
-    let cont;
-
-    do {
-        cont = prompt("Would you like to play again? (Y/N)").toLowerCase();
-
-        if (cont === 'n') {
-            console.log('Thanks for playing! Goodbye!');
-            console.log('☉ ‿ ⚆');
+        if (isNaN(playerSelection)) {
+            console.log('You must choose a number corresponding to your choice...');
+            playerSelection = parseInt(prompt('Type in the number of your choice: (1) Rock, (2) Paper, or (3) Scissors'));
+        } else if (playerSelection === 1) {
+            playerSelection = 'Rock';
             break;
-        } else if (cont === 'y') {
-            console.log('As you wish...');
+        } else if (playerSelection === 2) {
+            playerSelection = 'Paper';
+            break;
+        } else if (playerSelection === 3) {
+            playerSelection = 'Scissors';
             break;
         } else {
-            console.log('¯\\(°_o)/¯');
-            console.log("Huh? You must enter 'Y' for YES or 'N' for NO. ");
+            console.log('Huh? Your choice must be a number 1-3.');
+            playerSelection = parseInt(prompt('Type in the number of your choice: (1) Rock, (2) Paper, or (3) Scissors'));
         }
-    } while (true);
+    }
 
+    const computerSelection = getComputerChoice();
+
+    if (playerSelection === computerSelection) {
+        return `IT'S A TIE! ${playerSelection} ties with ${computerSelection}`;
+    } else if (
+        (playerSelection === 'Rock' && computerSelection === 'Scissors') ||
+        (playerSelection === 'Paper' && computerSelection === 'Rock') ||
+        (playerSelection === 'Scissors' && computerSelection === 'Paper')
+    ) {
+        playerScore++;
+        return `YOU WON! ${playerSelection} beats ${computerSelection}`;
+    } else {
+        computerScore++;
+        return `YOU LOST! ${computerSelection} beats ${playerSelection}`;
+    }
 }
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        roundCounter++;
+        playRound();
+    }
+
+    if (playerScore > computerScore) {
+        return `Your score: ${playerScore}. Computer's score: ${computerScore}. YOU WON THE GAME!`;
+    } else if (computerScore > playerScore) {
+        return `Your score: ${playerScore}. Computer's score: ${computerScore}. YOU LOST. THE COMPUTER WON THE GAME!`;
+    } else {
+        return `Your score: ${playerScore}. Computer's score: ${computerScore}. YOU TIED THE GAME!`;
+    }
+}
+
+console.log(game());
